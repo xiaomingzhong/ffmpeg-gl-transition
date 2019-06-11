3ab7f6c50a10eb0669d6882565ab98594f117053
xiaoming <845393842@qq.com>
2019-06-11 16:44:46 +0800

    2019-06-11 16:44:45

--- tmp.sh
+++ tmp.sh
@@ -17,20 +17,23 @@ function get_video_second(){
 	echo $[${h}*3600 + ${m}*60 + s ]  # 00 * 02 * 39
 }
 
-function build_complex(){
-	file_list=$1
-	flen=${#file_list[@]}
-	
-	for f in $file_list
+function build_input(){
+	for f in $1
 	do
     	  echo " -i "${f}" "
 	done
+}
 
-	echo " -filter_complex \" "
+function build_map(){
+	echo " -map \"[$1]\" "
+}
 
+
+function build_filter_args(){
+	file_list=$1
 	# audio list 
 	flag=0
-	for f in $file_list
+	for f in ${file_list}
 	do
 	  video_second=$(get_video_second ${f})
 	  split_second=$[${video_second} - ${transition_time}]
@@ -40,7 +43,7 @@ function build_complex(){
 
 	# video list 
 	flag=0
-	for f in $file_list
+	for f in ${file_list}
 	do
     	  echo " [${flag}:v]split["vf"${flag}]["vb"${flag}]; "
 	  flag=$[${flag} + 1]
@@ -48,7 +51,7 @@ function build_complex(){
 
 	# split video fragment 
 	flag=0
-	for f in $file_list
+	for f in ${file_list}
 	do
 	  video_second=$(get_video_second ${f})
 	  split_second=$[${video_second} - ${transition_time}]
@@ -59,66 +62,60 @@ function build_complex(){
 
 	# concat gltransition video 
 	flag=0
-	for f in $file_list
-	do
-	  	if [ $[${flag}] -le $[${flen}] ]; then # flag < flen
-          behind=$[${flag} + 1]
+	size=${#video_list[@]}
+	last_flag=$[${size} - 1]
+	for ((i=0;i<last_flag;i++))
+	{
+      behind=$[${flag} + 1]
+	  gl_len=${#gltransition_list[@]}
+	  rdm=$(random_number ${gl_len})
+	  gltransition_name=${gltransition_list[${rdm}]}
 		  
-		  gl_len=${#gltransition_list[@]}
-		  rdm=$(random_number ${gl_len})
-		  gltransition_name=${gltransition_list[${rdm}]}
-		  
-    	  echo " ["vbb"${flag}]["vff"${behind}]gltransition=duration=${transition_time}:source=${transition_path}/${gltransition_name}["gl"${flag}]; "
-	      flag=$[${flag} + 1]
-      	fi 
-	done 
+      echo " ["vbb"${flag}]["vff"${behind}]gltransition=duration=${transition_time}:source=${transition_path}/${gltransition_name}["gl"${flag}]; "
+	  flag=$[${flag} + 1]
+    }
 	
 	# concat all video fragment
 	flag=0
-	size=0
-	all_fragment=
-	for f in $file_list
-	do
-	  	if [ $[${flag}] == '0' ]; then  # flag < flen
-          all_fragment="["vff"${flag}]"
-		  all_fragment=${all_fragment}"["gl"${flag}]"
-		  flag=$[${flag} + 1]
-		  size=$[${size} + 2]
-		elif [ $[${flag}] -le $[${flen}] ]; then  
-    	  all_fragment=${all_fragment}"["gl"${flag}]"
-	      flag=$[${flag} + 1]
-		  size=$[${size} + 1]
-		else 
-		  all_fragment=${all_fragment}"["vbb"${flag}]"
-		  flag=$[${flag} + 1]
-		  size=$[${size} + 1]
-      	fi 
-	done 
-	echo ${all_fragment}"concat=n=${size} [v]; "
+	all_fragment="["vff"${flag}]"
+	vsize=0
+	vsize=$[${vsize} + 1]
+	
+	for ((i=0;i<last_flag;i++))
+	{
+	  all_fragment=${all_fragment}"["gl"${flag}]"
+	  flag=$[${flag} + 1]
+	  vsize=$[${vsize} + 1]
+    }
+	
+	all_fragment=${all_fragment}"["vbb"${flag}]"
+	vsize=$[${vsize} + 1]
+	
+	echo ${all_fragment}"concat=n=${vsize} [v]; "
 
 	# concat all audio fragment
 	flag=0
 	all_fragment=
-	for f in $file_list
+	for f in ${file_list}
 	do
 	  all_fragment=${all_fragment}"["a"${flag}]"
  	  flag=$[${flag} + 1]
 	done
-    echo ${all_fragment}"concat=n=${flag}:v=0:a=1[audio] \""
-	echo " -map \"[v]\" -map \"[audio]\" "
-	echo " -vcodec libx264 -crf 23 -preset medium -acodec aac -strict experimental -ac 2 -y ${out_video_name}"
+    echo ${all_fragment}"concat=n=${flag}:v=0:a=1[audio] "
 }
 
 function ffmpeg_build(){
-	echo `ffmpeg  "$(build_complex $1)"`
+	echo `ffmpeg  "$(build_filter_args $1)"`
 }
 
 
-transition_time=2 # 3s
+transition_time=1 # 3s
 transition_path=/home/xiaoming/Downloads/youtube/30s/transitions
+video_path=/home/xiaoming/Downloads/youtube/30s/source
 gltransition_name=
 out_video_name="out.mp4"
 gltransition_list=()
+video_list=()
 
 # *******************load glsl files*******************************
 	index=0
@@ -129,10 +126,28 @@ gltransition_list=()
 	done
 # *****************************************************************
 
-paths=("/home/xiaoming/Downloads/youtube/30s/1.mp4" "/home/xiaoming/Downloads/youtube/30s/2.mp4" "/home/xiaoming/Downloads/youtube/30s/0.mp4")
-ffmpeg_args=$(build_complex "${paths[*]}")
+# *******************load video files*******************************
+	index=0
+	for f in ${video_path}/*.mp4
+	do
+	  video_list[index]=${f}
+	  index=$[${index} + 1]
+	done
+# *****************************************************************
 
-echo "ffmpeg"${ffmpeg_args}
+# paths=("/home/xiaoming/Downloads/youtube/30s/1.mp4" "/home/xiaoming/Downloads/youtube/30s/2.mp4" "/home/xiaoming/Downloads/youtube/30s/0.mp4")
+input=$(build_input "${video_list[*]}")
+
+filter_complex=" -filter_complex "
+
+filter_args=$(build_filter_args "${video_list[*]}")
+
+map_v=$(build_map "v")
+map_audio=$(build_map "audio")
+
+vcodec_arg=" -vcodec libx264 -crf 23 -preset medium -acodec aac -strict experimental -ac 2 -y ${out_video_name}"
+
+echo "ffmpeg"${input}${filter_complex}"\""${filter_args}"\""${map_v}${map_audio}${vcodec_arg}
 
 
 
